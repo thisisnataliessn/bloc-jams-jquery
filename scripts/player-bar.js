5153e86f6a1d3fce90323443cc99ee3476adf9cb
Natalie Zdan <natalie.zdan@gmail.com>
2019-01-09 13:43:37 -0600

    Checkpoint 11 completed: time slider

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -3,6 +3,7 @@
        player.playPause(); 
        $(this).attr('playState', player.playState);
     });
+    
     $('button#next').on("click", function() {
         if (player.playState !== 'playing') {return;}
         const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
@@ -11,6 +12,7 @@
         const nextSong = album.songs[nextSongIndex];
         player.playPause(nextSong);
     });
+
     $('button#previous').on("click", function() {
         if (player.playState !== 'playing') {return;}
         const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
@@ -19,4 +21,17 @@
         const previousSong = album.songs[previousSongIndex];
         player.playPause(previousSong);
     });
+
+    $("#time-control input").on('input', function() {
+        player.skipTo(event.target.value);
+    });
+
+    setInterval( () => {
+        if (player.playState !== "playing") {return;}
+        const currentTime = player.getTime();
+        const duration = player.getDuration();
+        const percent = (currentTime/duration) * 100;
+        $('#time-control .current-time').text(currentTime);
+        $('#time-control input').val(percent);
+    }, 1000)
 }
